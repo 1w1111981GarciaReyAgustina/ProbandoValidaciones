f3ed2f95f3d357fb3fc39a26186ff237fab3d050
1w1111981GarciaReyAgustina <69599662+1w1111981GarciaReyAgustina@users.noreply.github.com>
2020-09-15 00:39:22 -0300

    primer commit

---
+++ frmDatos.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Validaciones
+{
+    partial class frmDatos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.picPass = new System.Windows.Forms.PictureBox();
+            this.picMail = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(123, 41);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(123, 76);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 2;
+            this.txtApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellido_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Edad";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(123, 114);
+            this.txtEdad.MaxLength = 4;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 3;
+            this.txtEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdad_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "E-mail";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(123, 152);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(100, 20);
+            this.txtMail.TabIndex = 4;
+            this.txtMail.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMail_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Contraseña";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(123, 190);
+            this.txtPass.MaxLength = 16;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 5;
+            this.txtPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPass_KeyPress);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(25, 277);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(82, 31);
+            this.btnIngresar.TabIndex = 7;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(147, 277);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(86, 31);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Promedio";
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(123, 228);
+            this.txtPromedio.MaxLength = 5;
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(100, 20);
+            this.txtPromedio.TabIndex = 6;
+            this.txtPromedio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPromedio_KeyPress);
+            // 
+            // picPass
+            // 
+            this.picPass.Image = global::Validaciones.Properties.Resources.pngegg;
+            this.picPass.Location = new System.Drawing.Point(229, 191);
+            this.picPass.Name = "picPass";
+            this.picPass.Size = new System.Drawing.Size(18, 19);
+            this.picPass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPass.TabIndex = 3;
+            this.picPass.TabStop = false;
+            this.picPass.Visible = false;
+            // 
+            // picMail
+            // 
+            this.picMail.Image = global::Validaciones.Properties.Resources.pngegg;
+            this.picMail.Location = new System.Drawing.Point(229, 155);
+            this.picMail.Name = "picMail";
+            this.picMail.Size = new System.Drawing.Size(18, 19);
+            this.picMail.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMail.TabIndex = 3;
+            this.picMail.TabStop = false;
+            this.picMail.Visible = false;
+            // 
+            // frmDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 320);
+            this.Controls.Add(this.picMail);
+            this.Controls.Add(this.picPass);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDatos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos";
+            this.Load += new System.EventHandler(this.frmDatos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.PictureBox picPass;
+        private System.Windows.Forms.PictureBox picMail;
+    }
+}
+
---
+++ frmDatos.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions; //Regex
+
+namespace Validaciones
+{
+    public partial class frmDatos : Form
+    {
+        string patternPass;
+        string patternMail; 
+        public frmDatos()
+        {
+            InitializeComponent();
+            //Validar que la contraseña ingresada es correcta. Para esto necesito un string input y un string pattern
+            //Mi pattern establece que la contraseña tenga al menos 1 letra mayúscula, 1 minúscula, 1 número y que su extensión sea entre 8 y 16 caracteres.
+            //Mini resumen de lo que significan estas expresiones regulares 
+            //^ y $ son mis anclas (Anchors specify a position in the string where a match must occur)
+            //(?= subexpression ) es mi grouping construct
+            //* y {n,m} son los quantifiers aquí utilizo:
+            //* Matches the previous element zero or more times, acá también se podría utilizar el quantifier +
+            //{n,m} Matches the previous element at least n times, but no more than m times
+            //. [n-m] y \d son character classes
+            //El . es como mi wildcard
+            //[character_group] Matches any single character in character_group.By default, the match is case-sensitive.
+            //\d Matches any decimal digit.
+            //IR PROBANDO MI PATTERN en https://regex101.com/)
+            patternPass = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,16}$";
+            patternMail = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Usted está a punto de salir. ¿Está seguro?", "SALIR", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Validaciones.Letras(e);
+        }
+
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Validaciones.Letras(e);
+        }
+
+        private void txtPromedio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Validaciones.NumerosDecimales(e);
+        }
+
+        private void frmDatos_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Validaciones.NumerosEnteros(e);
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+            if (Regex.IsMatch(txtPass.Text, patternPass)==false)
+            {
+                MessageBox.Show("La contraseña no es válida. Debe contener al menos una letra mayúscula, una mínuscula y un número. Su extensión debe ser de 8 a 16 caracteres", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+            if (Regex.IsMatch(txtMail.Text, patternMail) == false)
+            {
+                MessageBox.Show("El mail ingresado no es válido", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            if (Regex.IsMatch(txtPass.Text, patternPass) && Regex.IsMatch(txtMail.Text, patternMail))
+            {
+                MessageBox.Show("Usuario ingresado con éxito", "AVISO", MessageBoxButtons.OK);
+                txtNombre.Clear();
+                txtApellido.Clear();
+                txtEdad.Clear();
+                txtPass.Clear();
+                txtMail.Clear();
+                txtPromedio.Clear();
+            }
+        }
+
+        private void txtMail_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Regex.IsMatch(txtMail.Text, patternMail))
+            {
+                picMail.Visible = true;
+                if (e.KeyChar == Convert.ToChar(Keys.Back))
+                {
+                    picMail.Visible = false;
+                }
+            }
+        }
+
+        private void txtPass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Regex.IsMatch(txtPass.Text, patternPass))
+            {
+                picPass.Visible = true;
+                if (e.KeyChar == Convert.ToChar(Keys.Back))
+                {
+                    picPass.Visible = false;
+                }
+            }
+        }
+    }
+}
